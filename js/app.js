b644be36bea67a9953e4a3a2c94fd7e104c5fd1b
Umesh <kuwar51@yahoo.com>
2021-04-29 23:30:01 +0530

    design added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function submitform(e){
 e.preventDefault();
 var name=document.forms["welcome_form"]["name"].value;
-sessionStorage.setItem("name", name);   
-console.log(form_data);
+sessionStorage.setItem("name", name);
+location.href="quiz.html";  
+
 }
